0adf87a8391c5ded9118247e744d30c3a93c74a4
felixlaryssa <felixllaryssa@gmail.com>
2024-11-29 10:47:44 -0300

    secao favoritos

---
+++ src/components/FavoriteList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import MovieCard from './MovieCard'; componente com informacoes sobre o filme
+
+const FavoriteList = ({ favorites, onRemoveFavorite }) => {
+    return (
+        <div className="favorite-list">
+        {favorites.map((movie) => (
+            <MovieCard
+            key={movie.id}
+            movie={movie}
+            onRemoveFavorite={onRemoveFavorite}
+            isFavorite={true}
+            />
+        ))}
+        </div>
+    );
+};
+
+export default FavoriteList;
